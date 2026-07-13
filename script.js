85ce092c6b8fec908457da3eda2cc7df36057dae
Jiji-Pay <jackdumont157@gmail.com>
2026-07-13 13:54:06 +0100

    Replace payer function with payerWave for payment redirect
    Removed the original payer function and replaced it with payerWave function that redirects to the payment URL.

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-
-function payer() {
-
-    const vendeur = document.querySelectorAll("input")[0].value;
-    const article = document.querySelectorAll("input")[1].value;
-    const prix = document.querySelectorAll("input")[2].value;
-    const nom = document.querySelectorAll("input")[3].value;
-    const telephone = document.querySelectorAll("input")[4].value;
-
-    if (
-        vendeur === "" ||
-        article === "" ||
-        prix === "" ||
-        nom === "" ||
-        telephone === ""
-    ) {
-        alert("Veuillez remplir tous les champs.");
-        return;
-    }
-
-    window.location.assign("https://pay.wave.com/m/M_ci_KSgOfOLiIMqV/c/ci/");
+function payerWave() {
+  window.location.href = "https://pay.wave.com/m/M_ci_KSgOfOLiIMqV/c/ci/";
 }
