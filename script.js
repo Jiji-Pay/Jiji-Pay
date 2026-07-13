ab4b225802bd416a6f025fef6fcf2598500d49f6
Jiji-Pay <jackdumont157@gmail.com>
2026-07-13 13:40:32 +0100

    Implement payer function for input validation and redirection
    Added payer function to validate input fields and redirect to payment page.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+function payer() {
+
+    const vendeur = document.querySelectorAll("input")[0].value;
+    const article = document.querySelectorAll("input")[1].value;
+    const prix = document.querySelectorAll("input")[2].value;
+    const nom = document.querySelectorAll("input")[3].value;
+    const telephone = document.querySelectorAll("input")[4].value;
+
+    if (
+        vendeur === "" ||
+        article === "" ||
+        prix === "" ||
+        nom === "" ||
+        telephone === ""
+    ) {
+        alert("Veuillez remplir tous les champs.");
+        return;
+    }
+
+    window.location.assign("https://pay.wave.com/m/M_ci_KSgOfOLiIMqV/c/ci/");
+}
